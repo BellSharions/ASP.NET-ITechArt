dad5f717a175d5d63f5be1e1d9036f6afeb4ea67
Yury <bellsharions@gmail.com>
2021-09-30 19:41:17 +0300

    First commit

---
+++ ASP-NET/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace ASP_NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string GetInfo()
+        {
+            return "Hello World!";
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
