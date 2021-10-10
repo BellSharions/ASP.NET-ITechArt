6c326b0076a163090327e1a903be64c43e7e8e52
BellSharions <bellsharions@gmail.com>
2021-10-10 22:01:16 +0300

    Added phone validation

--- ASP-NET/Models/UserModel.cs
+++ ASP-NET/Models/UserModel.cs
@@ -1,10 +1,12 @@
 ﻿using DAL.Entities.Roles;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP_NET.Models
 {
     public class UserModel //future implementation is required
     {
         public string UserName {  get; set; }
+        [Phone]
         public string PhoneNumber {  get; set; }
         public string Email { get; set; }
         public string Password {  get; set; }
